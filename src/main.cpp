b9e4dd86910fc4e0b5f0728589ae3859dc33c805
Befikir Shimelis <161409855+squidward404@users.noreply.github.com>
2026-07-06 16:23:05 +0300

    added the black-hole class containing the draw circle function and a constructor

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,42 @@
 
 #include <cmath> //need sqrt() and probably trig functions.
 
-/*constants will be here*/
+/*classes*/
+class BlackHole{
+public:
 
+//variables 
+float mass;
+float radius;
 
+BlackHole(float m) {
+        mass=m;
+        radius=2*mass; //schwarzschild radius(radius of the black hole is 2x of it's mass)
+    }
+
+//draw the circle for the hole
+void DrawCircle() {
+        glBegin(GL_TRIANGLE_FAN);
+        glColor3f(0,0,0);//black Color
+        glVertex2f(0,0);//center
+
+        //draw 100 segments for a smooth circle
+        for (int i=0;i<=100;i++) {
+            float angle =2*3.14159265*(float)i /100;
+            float x = cos(angle) * radius;
+            float y = sin(angle) * radius;
+            glVertex2f(x, y);
+        }
+        glEnd();
+    }
+};
+
+class Rays{
+public:
+
+};
+
+/*constants*/
 int main(){
 //start GLFW library
     if (!glfwInit()) {
@@ -17,7 +50,7 @@ int main(){
         return -1;
     }
 //create an 800x800 pixel window titled " Black Hole"
-    GLFWwindow* window = glfwCreateWindow(800, 800, "Black Hole", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(900,800,"Black Hole",NULL,NULL);
 
 //make this window the active one for drawing
     glfwMakeContextCurrent(window);
@@ -27,8 +60,11 @@ int main(){
         std::cerr << "Failed to init GLEW" << std::endl;
         return -1;
     }
-// Set the background color to Black (R=0, G=0, B=0)
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+    //dark space blue grey(R=0.05,G=0.05,B=0.1)
+    glClearColor(0.05f,0.05f,0.1f,1.0f);    
+    BlackHole black_hole(0.1);
+    Rays ray;
 
     while (!glfwWindowShouldClose(window)) {
 
@@ -36,6 +72,7 @@ int main(){
         glClear(GL_COLOR_BUFFER_BIT);
 
         /*The main code*/
+        black_hole.DrawCircle();
 
         //swap the back buffer
         glfwSwapBuffers(window);
