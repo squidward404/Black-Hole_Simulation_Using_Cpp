b7c54b03c4ec290b54eb04dbbb5237d2670dddc6
Befikir Shimelis <161409855+squidward404@users.noreply.github.com>
2026-07-08 09:10:21 +0300

    added the calculations for gravity and bending intensity

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,14 @@
 
 #include <iostream>
 #include <vector>
-#include <cmath> //need sqrt() and probably trig functions.
+#include <cmath> //need sqrt() and trig functions(to draw the circle from triangles coz OpenGL doesn't have a native DrawCircle()).
 
 /*classes*/
 class BlackHole{
 public:
 
 //variables 
-float mass=0.1;
-
+float mass=0.05;
 float radius=2*mass; //schwarzschild radius(radius of a black hole is 2x of it's mass)
 
 //draw the circle for the hole
@@ -46,7 +45,7 @@ std::vector<glm::vec2> path; //store history of positions
 //constractor function 
 Rays(float startX, float startY) {
         pos = glm::vec2(startX,startY);
-        dir = glm::vec2(1,0); // Move along the x-axis (to the Right)
+        dir = glm::vec2(1,0); // Move along the x-axis(to the right)
         active = true;
         path.push_back(pos); //save the starting point
     }
@@ -68,19 +67,36 @@ void draw() {
     }
 
 
-//controll movement of the light rays relative to position
-void update(){
+//controll movement of the light rays relative to position and bending due to the bh
+void update(float mass_of_the_bh,glm::vec2 position_of_the_bh){
     if (!active) return;
+
+    glm::vec2 toBH=position_of_the_bh-pos; //find direction to the hole
+    float distSq=toBH.x*toBH.x+toBH.y*toBH.y; //the distance^2 to calculate gravity
+    float dist=sqrt(distSq);  //the actual distance     
+    float black_hole_radius=2.0f*mass_of_the_bh;  //the radius of the black hole
+    //don't draw over the black hole
+    if(dist<black_hole_radius){
+        active=false;
+        return;
+    }
+    float force=mass_of_the_bh/distSq; //gravity(small distance=strong pull,large distance=weaker pull)
+    glm::vec2 directionToBH =toBH/dist;//I don't understand this(google search)   
+    glm::vec2 acceleration=directionToBH*force;//acceleration vector
+    dir=dir+acceleration*0.01f;//intensity of bending due to acceleration(had to assume 0.01 coz don't want to do Runge-Kutta integration)
+    //didn't understand this one too.I guess it has smt to do with bending angle correction(google search)
+    float len=sqrt(dir.x*dir.x+dir.y*dir.y);
+    dir =dir/len;   
     pos = pos+dir*0.005f; 
-    path.push_back(pos);
-    if (pos.x > 1.2f) {
-            active = false;
-        }
+    path.push_back(pos);//save the trace  
+
+   if (pos.x>1.2||pos.y>1.2||pos.y<-1.2){
+        active = false;
+    }
 
 }
 
 };
-
 
 int main(){
 //start GLFW library
@@ -107,22 +123,25 @@ int main(){
     BlackHole black_hole;
     //store the multiple rays inside a vector
     std::vector<Rays> all_rays;
-    for (int i=0;i<70;i++){
-        float startY=-0.8+(i*0.03); //heights from -0.8 to 0.8
+    all_rays.clear();
+    for (int i=0;i<100;i++){
+        float startY=-4.0+(i*0.05); //heights from -0.8 to 0.8
         all_rays.push_back(Rays(-1.5f, startY)); //start further left (-1.5)
     }
-
-    while (!glfwWindowShouldClose(window)) {
+        while (!glfwWindowShouldClose(window)) {
 
         //clear the screen 
         glClear(GL_COLOR_BUFFER_BIT);
 
         /*The main code*/
+
         //draw the circle
         black_hole.DrawCircle();
+
         //print all rays in a loop
         for(int i=0;i<all_rays.size();++i){
-        all_rays[i].update();
+        //pass mass (0.1f) and position (0,0)
+        all_rays[i].update(0.05f,glm::vec2(0.0f,0.0f));   
         all_rays[i].draw();
         }
         //swap the back buffer
